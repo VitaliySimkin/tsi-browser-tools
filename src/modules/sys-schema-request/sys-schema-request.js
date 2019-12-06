d335ea4f4e17194eb9972baf4b96204a29a420c1
VitaliySimkin <vitaliy.simkin@gmail.com>
2019-12-06 18:05:56 +0200

    [config] eslint and module build

--- src/modules/sys-schema-request/sys-schema-request.js
+++ src/modules/sys-schema-request/sys-schema-request.js
@@ -6,83 +6,83 @@ function getData() {
 	/** Get request body */
 	function getRequestBody() {
 		return {
-			"rootSchemaName": "SysSchema",
-			"operationType": 0,
-			"filters": {
-				"items": {
-					"ExtendParentFilter": {
-						"filterType": 1,
-						"comparisonType": 3,
-						"isEnabled": true,
-						"trimDateTimeParameterToDate": false,
-						"leftExpression": {
-							"expressionType": 0,
-							"columnPath": "ExtendParent"
+			'rootSchemaName': 'SysSchema',
+			'operationType': 0,
+			'filters': {
+				'items': {
+					'ExtendParentFilter': {
+						'filterType': 1,
+						'comparisonType': 3,
+						'isEnabled': true,
+						'trimDateTimeParameterToDate': false,
+						'leftExpression': {
+							'expressionType': 0,
+							'columnPath': 'ExtendParent'
 						},
-						"rightExpression": {
-							"expressionType": 2,
-							"parameter": {
-								"dataValueType": 1,
-								"value": false
+						'rightExpression': {
+							'expressionType': 2,
+							'parameter': {
+								'dataValueType': 1,
+								'value': false
 							}
 						}
 					}
 				},
-				"logicalOperation": 0,
-				"isEnabled": true,
-				"filterType": 6
+				'logicalOperation': 0,
+				'isEnabled': true,
+				'filterType': 6
 			},
-			"columns": {
-				"items": {
-					"Id": {
-						"caption": "",
-						"orderDirection": 0,
-						"orderPosition": -1,
-						"isVisible": true,
-						"expression": {
-							"expressionType": 0,
-							"columnPath": "Id"
+			'columns': {
+				'items': {
+					'Id': {
+						'caption': '',
+						'orderDirection': 0,
+						'orderPosition': -1,
+						'isVisible': true,
+						'expression': {
+							'expressionType': 0,
+							'columnPath': 'Id'
 						}
 					},
-					"UId": {
-						"caption": "",
-						"orderDirection": 0,
-						"orderPosition": -1,
-						"isVisible": true,
-						"expression": {
-							"expressionType": 0,
-							"columnPath": "UId"
+					'UId': {
+						'caption': '',
+						'orderDirection': 0,
+						'orderPosition': -1,
+						'isVisible': true,
+						'expression': {
+							'expressionType': 0,
+							'columnPath': 'UId'
 						}
 					},
-					"Name": {
-						"caption": "",
-						"orderDirection": 0,
-						"orderPosition": -1,
-						"isVisible": true,
-						"expression": {
-							"expressionType": 0,
-							"columnPath": "Name"
+					'Name': {
+						'caption': '',
+						'orderDirection': 0,
+						'orderPosition': -1,
+						'isVisible': true,
+						'expression': {
+							'expressionType': 0,
+							'columnPath': 'Name'
 						}
 					}
 				}
 			},
-			"isDistinct": false,
-			"rowCount": -1,
-			"rowsOffset": -1,
-			"isPageable": false,
-			"allColumns": false,
-			"useLocalization": true,
-			"useRecordDeactivation": false,
-			"serverESQCacheParameters": {
-				"cacheLevel": 0,
-				"cacheGroup": "",
-				"cacheItemName": ""
+			'isDistinct': false,
+			'rowCount': -1,
+			'rowsOffset': -1,
+			'isPageable': false,
+			'allColumns': false,
+			'useLocalization': true,
+			'useRecordDeactivation': false,
+			'serverESQCacheParameters': {
+				'cacheLevel': 0,
+				'cacheGroup': '',
+				'cacheItemName': ''
 			},
-			"queryOptimize": false,
-			"useMetrics": false,
-			"querySource": 0,
-			"ignoreDisplayValues": false,
-			"isHierarchical": false
+			'queryOptimize': false,
+			'useMetrics': false,
+			'querySource': 0,
+			'ignoreDisplayValues': false,
+			'isHierarchical': false
 		};
 	}
 
@@ -90,17 +90,17 @@ function getData() {
 	 * @param {Object} body body
 	 */
 	function getRequestParams(body) {
-		const coockie = (document.cookie.match(new RegExp("(^| )BPMCSRF=([^;]+)")) || [])[2];
-		if (typeof body !== "string") {
+		const coockie = (document.cookie.match(new RegExp('(^| )BPMCSRF=([^;]+)')) || [])[2];
+		if (typeof body !== 'string') {
 			body = JSON.stringify(body);
 		}
 		return {
-			"method": "POST",
-			"headers": {
-				"Content-Type": "application/json",
-				"BPMCSRF": coockie,
-				"Timestamp": new Date().toISOString(),
-				"X-Request-Source": "ajax-provider"
+			'method': 'POST',
+			'headers': {
+				'Content-Type': 'application/json',
+				'BPMCSRF': coockie,
+				'Timestamp': new Date().toISOString(),
+				'X-Request-Source': 'ajax-provider'
 			},
 			body
 		};
@@ -110,8 +110,8 @@ function getData() {
 	const requestParams = getRequestParams(body);
 	const requestChecker = r => r.ok ? r.json() : Promise.resolve(null);
 	return Promise.all([
-		fetch("./DataService/json/SyncReply/SelectQuery", requestParams).then(requestChecker),
-		fetch("../DataService/json/SyncReply/SelectQuery", requestParams).then(requestChecker)
+		fetch('./DataService/json/SyncReply/SelectQuery', requestParams).then(requestChecker),
+		fetch('../DataService/json/SyncReply/SelectQuery', requestParams).then(requestChecker)
 	]).then(r => {
 		const result = r.find(Boolean);
 		return result && result.rows || [];
--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,26 @@
-const path = require("path");
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	pages: {
 		index: {
-			entry: "src/popup/main.ts",
-			template: "src/popup/index.html",
-			filename: "popup/index.html",
-			title: "TSI Browser Tools"
+			entry: 'src/popup/main.ts',
+			template: 'src/popup/index.html',
+			filename: 'popup/index.html',
+			title: 'TSI Browser Tools'
 		}
 	},
 	filenameHashing: false,
-	assetsDir: "popup/assets",
-	outputDir: "dist/",
+	assetsDir: 'popup/assets',
+	outputDir: 'dist/',
 	configureWebpack: config => {
 		config.devtool = 'source-map';
 		config.plugins.push(new CopyWebpackPlugin([
 			{ from: 'src/img/', to: 'img/' },
 			{ from: 'src/chrome.manifest.json', to: 'manifest.json' },
 			{ from: 'src/background/background.js', to: 'background/background.js' },
-			{ from: 'src/features/', to: 'features/', ignore: ["*.ts"] }
+			{ from: 'src/features/', to: 'features/', ignore: ['*.ts'] },
+			{ from: 'src/modules/', to: 'modules/', ignore: ['*.ts'] }
 		]));
 	}
 }
