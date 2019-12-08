929886e028f7a07aa34878cc251cb39ad456a700
VitaliySimkin <vitaliy.simkin@gmail.com>
2019-12-09 00:33:56 +0200

    [features]/[bpm-column-info]

--- src/features/bpm-column-info/bpm-column-info.js
+++ src/features/bpm-column-info/bpm-column-info.js
@@ -1,164 +1,185 @@
-(function() {
-try {
-	/**
-	 * @typedef TsiColumnInfoConfig
-	 * @property {string} bindTo value bind
-	 * @property {Object} model model
+import NSModalBox from "../../modules/ns-modal-box/ns-modal-box.js";
+
+var NSBusinessRuleColumnInfo = {
+	getModelRules(model) {
+		var rules = [];
+		model = model.__proto__;
+		while (model) {
+			model.rules && !Terrasoft.isEmptyObject(model.rules) && rules.push(model.rules);
+			model = model.__proto__;
+		}
+		return rules.reduce((item, result) => this.mergeRules(result, item), {});
+	},
+	mergeRules(rules, businessRules) {
+		rules = rules || {};
+		businessRules = businessRules || {};
+		var result = {};
+		var columnNames = Ext.Array.merge(Ext.Object.getKeys(rules), Ext.Object.getKeys(businessRules));
+		Terrasoft.each(columnNames, function(columnName) {
+			result[columnName] = this.mergeColumnRules(rules[columnName], businessRules[columnName]);
+		}, this);
+		return result;
+	},
+	mergeColumnRules(columnRules, columnBusinessRules) {
+		columnRules = columnRules || {};
+		columnBusinessRules = columnBusinessRules || {};
+		var result = {};
+		var ruleNames = Ext.Array.merge(
+			Ext.Object.getKeys(columnRules),
+			Ext.Object.getKeys(columnBusinessRules));
+		Terrasoft.each(ruleNames, function(ruleName) {
+			result[ruleName] = Ext.apply(columnRules[ruleName] || {}, columnBusinessRules[ruleName]);
+		}, this);
+		return result;
+	},
+	async getBusinessRuleModule() {
+		return new Promise((callback) => {
+			require(["BusinessRuleModule"], function(BusinessRuleModule) {callback(BusinessRuleModule);});
+		});
+	},
+	async getColumnBindRules(model, columnName) {
+		let BusinessRuleModule = await this.getBusinessRuleModule();
+		let rules = this.getModelRules(model)[columnName] || {};
+		let isBINDPARAMETER = rule => rule.ruleType === BusinessRuleModule.enums.RuleType.BINDPARAMETER;
+		let bindRules = Object.values(rules).filter(isBINDPARAMETER);
+		return bindRules.map(rule => {
+			let compr = cond => this.getPropNameByValue(Terrasoft.ComparisonType, cond.comparisonType);
+			let getCondDesc = cond => `{${cond.leftExpression.attribute} ${compr(cond)} ${cond.rightExpression.value}}`;
+			let separaor = this.getPropNameByValue(Terrasoft.LogicalOperatorType, rule.logical) || "AND";
+			let value = rule.conditions.map(getCondDesc).join(` ${separaor} `);
+			let caption = this.getPropNameByValue(BusinessRuleModule.enums.Property, rule.property);
+			return {caption, value};
+		});
+	},
+	getPropNameByValue(obj, value) {
+		return _.invert(obj)[value];
+	}
+};
+
+const ColumnInfoManager = {
+	/** propsConfig */
+	props: [
+		{name: "caption", caption: "caption"},
+		{name: "columnPath", caption: "columnPath"},
+		{name: "dataValueType", caption: "DataType", propOf: "DataValueType"},
+		{name: "referenceSchemaName", caption: "referenceSchema"},
+		{name: "type", caption: "Type", propOf: "ViewModelColumnType"},
+		{name: "uId", caption: "UID"},
+		{name: "name", caption: "pageName", isModelProp: true}
+	],
+
+	/** Initialize */
+	init() {
+		let scope = this;
+		let initDomEvents = Terrasoft.BaseEdit.prototype.initDomEvents;
+		Terrasoft.BaseEdit.prototype.initDomEvents = function() {
+			scope.appendColumnInfoClickHandler.call(scope, this);
+			initDomEvents.apply(this, arguments);
+		};
+	},
+
+	/** Append column info click handler */
+	appendColumnInfoClickHandler(context) {
+		let wrapEl = context.getWrapEl()
+		wrapEl.on("click", function(event) {
+			this.onEditWrapElClick(context, event);
+		}, this);
+	},
+
+	/** Handle element click
+	 * @param {Object} context el viewmodel
+	 * @param {MouseEvent} event click event
 	 */
-	
-	const TsiColumnInfo = {
-		props: [
-			{name: "caption", caption: "caption"},
-			{name: "columnPath", caption: "columnPath"},
-			{name: "dataValueType", caption: "DataType", propOf: "DataValueType"},
-			{name: "referenceSchemaName", caption: "referenceSchema"},
-			{name: "type", caption: "Type", propOf: "ViewModelColumnType"},
-			{name: "uId", caption: "UID"},
-			{name: "name", caption: "pageName", isModelProp: true}
-		],
+	onEditWrapElClick(context, event) {
+		if (!event.altKey) {
+			return;
+		}
+		event.stopEvent();
+		let config = {model: context.model, bindTo: context.bindings.value.modelItem};
+		this.displayColumnInfo(config);
+	},
 
-		init() {
-			this.extendTerrasoftBaseEdit();
-			this.container = this.createContainer();
-		},
+	async displayColumnInfo({model, bindTo}) {
+		let items = this.getItems({model, bindTo});
+		let cnt = this.createEl("table.ns-column-info-table");
+		items.forEach(item => this.renderItem(item, cnt), this);
+		let rules = await NSBusinessRuleColumnInfo.getColumnBindRules(model, bindTo);
+		rules.forEach(rule => this.renderItem(rule, cnt), this);
+		new NSModalBox({
+			content: cnt,
+			style: {width: "500px"},
+			caption: "Column info"
+		});
+	},
 
-		render(config, renderTo) {
-			let items = this.getItems(config);
-			let cnt = document.createElement("div");
-			cnt.className = "column-info-cnt";
-			items.forEach(item => this.renderItem(item, cnt), this);
-			renderTo.innerHTML = "";
-			renderTo.appendChild(cnt);
-		},
-
-		renderItem(item, renderTo) {
-			let cnt = document.createElement("div");
-			cnt.className = "column-info-item";
-
-			let name = document.createElement("span");
-			name.className = "column-info-prop-name";
-			name.innerText = String(item.caption);
-			cnt.appendChild(name);
-
-			let value = document.createElement("span");
-			value.className = "column-info-prop-value";
-			value.innerText = String(item.value);
-			cnt.appendChild(value);
-
-
-			let copy = document.createElement("span");
-			copy.className = "conlumn-info-prop-copy";
-			copy.addEventListener("click", function() {
-				TsiColumnInfo.copyValue(item.value);
-			});
-			cnt.appendChild(copy);
-
-			renderTo.appendChild(cnt);
-		},
-
-		generateHtml(config) {
-			let items = this.getItems(config);
-			let itemsHtml = items.map(item => this.generateItemHtml(item));
-			return `<div class="column-info-cnt">${itemsHtml.join("")}</div>`
-		},
-		generateItemHtml(item) {
-			return `<div class="column-info-item">
-				<span class="column-info-prop-name">${item.caption}</span>
-				<span class="column-info-prop-value">${item.value}</span>
-				<span class=""></span>
-			</div>`;
-		},
-		/** getItems
-		 * @param {TsiColumnInfoConfig} config конфиг */
-		getItems(config) {
-			let items = [];
-			let _value = config.model && config.model.get(config.bindTo);
-			let value = (_value && _value.value) ? _value.value : _value;
+	getItems(config) {
+		let items = [];
+		let _value = config.model && config.model.get(config.bindTo);
+		let value = (_value && _value.value) ? _value.value : _value;
+		items.push({
+			caption: "value",
+			value: value
+		});
+		if (typeof _value === "object") {
 			items.push({
-				caption: "value",
-				value: value
+				caption: "displayValue",
+				value: _value.displayValue
 			});
-			if (typeof _value === "object") {
-				items.push({
-					caption: "displayValue",
-					value: _value.displayValue
-				});
-			}
-			let column = config.model.columns[config.bindTo];
-			this.props.forEach(prop => {
-				let value = prop.isModelProp ? config.model[prop.name] : column[prop.name];
-				if (prop.propOf) {
-					let obj = Terrasoft[prop.propOf];
-					for (let key in obj) {
-						if (obj[key] === value) {
-							value = key;
-							break;
-						}
+		}
+		let column = config.model.columns[config.bindTo];
+		this.props.forEach(prop => {
+			let value = prop.isModelProp ? config.model[prop.name] : column[prop.name];
+			if (prop.propOf) {
+				let obj = Terrasoft[prop.propOf];
+				for (let key in obj) {
+					if (obj[key] === value) {
+						value = key;
+						break;
 					}
 				}
-				items.push({
-					caption: prop.caption,
-					value: value
-				})
-			});
-			return items;
-		},
-
-		// #region Terrasoft.BaseEdit override
-		extendTerrasoftBaseEdit() {
-			let scope = this;
-			let initDomEvents = Terrasoft.BaseEdit.prototype.initDomEvents;
-			Terrasoft.BaseEdit.prototype.initDomEvents = function() {
-				scope.appendColumnInfo.call(scope, this);
-				initDomEvents.apply(this, arguments);
-			};
-		},
-
-		appendColumnInfo(context) {
-			let wrapEl = context.getWrapEl()
-			wrapEl.on("click", function(event) {
-				this.onEditWrapElClick(context, event);
-			}, this);
-		},
-	
-		onEditWrapElClick(context, event) {
-			if (!event.altKey) {
-				return;
 			}
-			event.stopEvent();
-			let config = {model: context.model, bindTo: context.bindings.value.modelItem};
-			TsiColumnInfo.render(config, this.container);
-			this.container.style.display = "block";
-			this.handleDocumentClick();
-		},
+			items.push({
+				caption: prop.caption,
+				value: value
+			})
+		});
+		return items;
+	},
 
-		createContainer() {
-			var container = document.createElement("div");
-			container.className = "column-info-box-cnt";
-			container.id = "tsi-column-info-box-cnt";
-			container.style.display = "none";
-			document.body.appendChild(container);
-			return container;
-		},
+	createEl(tag, innerText = "", props = {}) {
+		let elTag = tag.split(".")[0];
+		let className = tag.split(".")[1];
+		let el = document.createElement(elTag);
+		if (className) el.className = className;
 
-		handleDocumentClick() {
-			let onClick = function() {
-				let clickedInsidePage = !!(event.target.closest("#tsi-column-info-box-cnt"));
-				if (clickedInsidePage) {
-					return;
-				}
-				document.removeEventListener("click", onClick);
-				TsiColumnInfo.container.style.display = "none";
-			};
-			document.addEventListener("click", onClick);
-		},
+		if (typeof innerText === "object") props = innerText;
+		else el.innerText = String(innerText);
 
-		async copyValue(value) {
-			await navigator.clipboard.writeText(value);
-		}
-	};
+		for (let key in props) tr[key] = props[key];
+		return el;
+	},
 
-	TsiColumnInfo.init();
-	TsiColumnInfo.b();
-} catch (err) {}
-})();
+	createCopyValueBtn(value) {
+		let copy =this.createEl("span.nsci-prop-copy");
+		copy.addEventListener("click", function() {
+			this.copyValue(value);
+		}.bind(this));
+		return copy;
+	},
+
+	renderItem(item, renderTo) {
+		let tr = this.createEl("tr.ns-column-info-prop-row");
+		tr.appendChild(this.createEl("th.nsci-prop-name", item.caption));
+		tr.appendChild(this.createEl("td.nsci-prop-value", item.value));
+		let copyTd = this.createEl("td");
+		copyTd.appendChild(this.createCopyValueBtn(item.value));
+		tr.appendChild(copyTd);
+		renderTo.appendChild(tr);
+	},
+
+	async copyValue(value) {
+		await navigator.clipboard.writeText(value);
+	}
+}
+
+ColumnInfoManager.init();
