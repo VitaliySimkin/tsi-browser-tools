a1898e553b2927b34c7207b05c3629463ee9c4ba
VitaliySimkin <vitaliy.simkin@gmail.com>
2019-12-09 15:44:21 +0200

    [features]/[bpm-parent-name] FIX

--- src/features/bpm-parent-name/bpm-parent-name.js
+++ src/features/bpm-parent-name/bpm-parent-name.js
@@ -5,8 +5,8 @@ getSchemaNames().then((schemaNames) => {
 	Terrasoft.ComboBox.prototype.loadData = function(data) {
 		if (this.columnName === "ParentSchemaUId") {
 			data.forEach(item => {
-				let schemaName = schemaNames.find(r => r.UId === item[0]).Name;
-				item[1] = schemaName ? `${schemaName} (${item[1]})` : item[1];
+				const schema = schemaNames.find(r => r.UId === item[0]);
+				item[1] = schema ? `${schema.Name} (${item[1]})` : item[1];
 			});
 		}
 		baseLoadData.apply(this, arguments);
